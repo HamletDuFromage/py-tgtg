50a408d524201a73a1cf93b8fdd1753aeac17a6e
HamletDuFromage <61667930+HamletDuFromage@users.noreply.github.com>
2024-08-19 19:56:31 +0200

    cli arguments for watcher.py

--- pytgtg/watcher.py
+++ pytgtg/watcher.py
@@ -1,11 +1,15 @@
-
 import threading
 import time
 import api
+import argparse
 from pprint import pprint
 
-from exceptions import (TgtgConnectionError, TgtgForbiddenError,
-                        TgtgLoggedOutError, TgtgUnauthorizedError)
+from exceptions import (
+    TgtgConnectionError,
+    TgtgForbiddenError,
+    TgtgLoggedOutError,
+    TgtgUnauthorizedError,
+)
 
 
 class TooGoodToGoWatcher:
@@ -26,7 +30,9 @@ class TooGoodToGoWatcher:
             print(repr(error))
             return False
 
-        input(f"The login email should have been sent to {self.api.getCredentials().get('email')}. Open the email on your PC and click the link. Don't open the email on a phone that has the TooGoodToGo app installed. That won't work. Press the Enter key when you clicked the link.")
+        input(
+            f"The login email should have been sent to {self.api.getCredentials().get('email')}. Open the email on your PC and click the link. Don't open the email on a phone that has the TooGoodToGo app installed. That won't work. Press the Enter key when you clicked the link."
+        )
         try:
             self.api.authPoll(polling_id)
             print("✔️ Successfully logged in!")
@@ -40,7 +46,9 @@ class TooGoodToGoWatcher:
             businesses = self.api.listFavoriteBusinesses().json()
             for item in businesses.get("mobile_bucket").get("items"):
                 if item.get("items_available", 0) > 0:
-                    print(f"{item.get('display_name')} (available: {item.get('items_available')})")
+                    print(
+                        f"{item.get('display_name')} (available: {item.get('items_available')})"
+                    )
         except TgtgConnectionError as error:
             print(repr(error))
 
@@ -49,7 +57,12 @@ class TooGoodToGoWatcher:
         orders = orders.json()
         for month in orders.get("orders_per_month"):
             for order in month.get("orders"):
-                print(order.get("time_of_purchase").split('T')[0], order.get("order_id"), order.get("state"), order.get("store_name"))
+                print(
+                    order.get("time_of_purchase").split("T")[0],
+                    order.get("order_id"),
+                    order.get("state"),
+                    order.get("store_name"),
+                )
 
     def listActiveOrders(self):
         orders = self.api.getActiveOrders()
@@ -57,7 +70,22 @@ class TooGoodToGoWatcher:
         for order in orders.get("orders"):
             print(order.get("order_id"), order.get("state"), order.get("store_name"))
 
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="TooGoodToGo Watcher")
+    parser.add_argument(
+        "--sleep",
+        type=int,
+        default=60,
+        help="Time to sleep between checks in seconds (default: 60)",
+    )
+    parser.add_argument(
+        "--count",
+        type=int,
+        default=200,
+        help="Number of iterations for checking (default: 200)",
+    )
+    args = parser.parse_args()
     watcher = TooGoodToGoWatcher()
     watcher.consoleLogin()
 
@@ -68,8 +96,10 @@ if __name__ == "__main__":
     pprint(watcher.api.getItemInfo(1170509).json().get("display_name"))
     c = 0
     while True:
+        print(f"Watched favourites {c} times")
         threading.Thread(target=watcher.listMatches())
         if c > 200 == 0:
             print("stopped watching")
             break
-        time.sleep(60)
+        time.sleep(args.sleep)
+        c += 1
