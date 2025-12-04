0d3dac8cd7fb0604cce138877781d75995edd3ca
flb <f.lb@gmx.com>
2025-12-04 17:09:27 +0100

    feat: log bad request errors

--- pytgtg/api.py
+++ pytgtg/api.py
@@ -140,7 +140,7 @@ class TooGoodToGoApi:
             message = f"Error {post.status_code} for post request {endpoint}"
             if post.status_code == 401:
                 raise TgtgUnauthorizedError(endpoint, message, post)
-            elif post.status_code = 400:
+            elif post.status_code == 400:
                 raise TgtgBadRequestError(endpoint, message, post)
             elif post.status_code == 403:
                 captcha = post.json().get("url", "")
